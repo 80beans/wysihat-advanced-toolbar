8d105c67e4982234b66960b0ca45c99bcf73426b
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2009-11-03 10:50:29 +0100

    Initial commit (copy of the original wysihat toolbar)

---
+++ src/wysihat/advanced_toolbar/button_sets.js
@@ -0,0 +1,21 @@
+/**
+ * WysiHat.Toolbar.ButtonSets
+ *
+ *  A namespace for various sets of Toolbar buttons. These sets should be
+ *  compatible with WysiHat.Toolbar, and can be added to the toolbar with:
+ *  toolbar.addButtonSet(WysiHat.Toolbar.ButtonSets.Basic);
+**/
+WysiHat.AdvancedToolbar.ButtonSets = {};
+
+/**
+ * WysiHat.Toolbar.ButtonSets.Basic
+ *
+ *  A basic set of buttons: bold, underline, and italic. This set is
+ *  compatible with WysiHat.Toolbar, and can be added to the toolbar with:
+ *  toolbar.addButtonSet(WysiHat.Toolbar.ButtonSets.Basic);
+**/
+WysiHat.AdvancedToolbar.ButtonSets.Basic = $A([
+  { label: "Bold" },
+  { label: "Underline" },
+  { label: "Italic" }
+]);
